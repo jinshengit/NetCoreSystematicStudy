5a3c420930ab9043d3a1143146ff8ffeea0ad589
jinshen <jinshen@aidaijia.com>
2017-10-11 18:03:14 +0800

    Some code for Tag helpers

---
+++ Cities/Infrastructure/TagHelpers/ButtonTagHelper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cities.Infrastructure.TagHelpers
+{
+    /// <summary>
+    /// Defining a Tag Helper for bootstrap button
+    /// </summary>
+    [HtmlTargetElement("button", Attributes = "bs-button-color", ParentTag = "form")] //定义Tag Helper的范围
+    [HtmlTargetElement("a", Attributes = "bs-button-color", ParentTag = "form")]
+    public class ButtonTagHelper : TagHelper
+    {
+        public string BsButtonColor { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.SetAttribute("class", $"btn btn-{BsButtonColor}");
+        }
+    }
+}
---
+++ Cities/README.txt
@@ -0,0 +1,3 @@
+﻿This project reveals how to use Tag helpers.
+
+Tag helpers are a new feature that has been introduced in ASP.NET Core MVC and are C# classes that transform HTML elements in a view.
